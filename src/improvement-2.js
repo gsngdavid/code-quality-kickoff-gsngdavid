e56ffbd2773e77d5f790da88cecf854dcce1ae4e
gsngdavid <gsngdavid@gmail.com>
2023-10-09 14:24:19 +0200

    refactor: Replace loop with filter method

--- src/improvement-2.js
+++ src/improvement-2.js
@@ -6,12 +6,5 @@
 
 // FUNCTION 2
 export default function processItems(items) {
-  const processedItems = [];
-  for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-    if (item.type === "Food") {
-      processedItems.push(item);
-    }
-  }
-  return processedItems;
+  return items.filter(item => item.type === 'Food');
 }
