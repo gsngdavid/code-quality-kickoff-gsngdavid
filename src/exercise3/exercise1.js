769e333cab4337f1ebe431171af23623c8fa49af
gsngdavid <gsngdavid@gmail.com>
2023-10-20 12:10:13 +0200

    refactor: exercise 1

--- src/exercise3/exercise1.js
+++ src/exercise3/exercise1.js
@@ -1,10 +1,4 @@
 function calculateAverage(numbers) {
-    var sum = 0;
-    var count = 0;
-    for (var i = 0; i < numbers.length; i++) {
-      sum += numbers[i];
-      count++;
-    }
-    var average = sum / count;
-    return average;
-  }
+    const sum = numbers.reduce((total, number) => total + number);
+    return sum / numbers.length;
+}
