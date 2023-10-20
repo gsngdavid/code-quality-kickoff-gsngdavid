6d24797d11cb7f0bd2ec2f543d1fec0dbd76287a
gsngdavid <gsngdavid@gmail.com>
2023-10-20 12:45:35 +0200

    refactor: exercise 8

--- src/exercise3/exercise8.js
+++ src/exercise3/exercise8.js
@@ -1,9 +1,4 @@
-function discount(totalamount) {
-    var disc;
-    if (totalamount > 100) {
-      disc = totalamount * 0.1;
-    } else {
-      disc = totalamount * 0.05;
-    }
-    return disc;
-  }
+function discount(totalAmount) {
+  if (totalAmount > 100) return totalAmount * 0.1;
+  return totalAmount * 0.05;
+}
