bbf378f1d5ee6ef03ff2c7dfd3fb121fb4836c3a
gsngdavid <gsngdavid@gmail.com>
2023-10-20 12:39:37 +0200

    refactor: exercise 6

--- src/exercise3/exercise6.js
+++ src/exercise3/exercise6.js
@@ -1,11 +1,7 @@
 function maxNumber(a, b, c) {
-    if (a > b && a > c) {
-      return a;
-    } else {
-      if (b > a && b > c) {
-        return b;
-      } else {
-        return c;
-      }
-    }
-  }
+  if (a > b && a > c) return a;
+  if (b > a && b > c) return b;
+  return c;
+}
+
+maxNumber(100, 100, 10); // Does not work
