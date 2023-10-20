9a9b092b3d21991474981dbef02dde11b1d22cea
gsngdavid <gsngdavid@gmail.com>
2023-10-20 12:20:55 +0200

    refactor: exercise 4

--- src/exercise3/exercise4.js
+++ src/exercise3/exercise4.js
@@ -1,10 +1,3 @@
-// Dirty code
 function findEvenNumbers(numbers) {
-    var evenNumbers = [];
-    for (var i = 0; i < numbers.length; i++) {
-      if (numbers[i] % 2 === 0) {
-        evenNumbers.push(numbers[i]);
-      }
-    }
-    return evenNumbers;
-  }
+  return numbers.filter((number) => number % 2 === 0);
+}
