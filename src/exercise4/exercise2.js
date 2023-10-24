39b229f0a3469400ee7a02c7da8a4c1200ad4f9f
gsngdavid <gsngdavid@gmail.com>
2023-10-24 12:31:59 +0200

    feat: implement function to check prime numbers

--- src/exercise4/exercise2.js
+++ src/exercise4/exercise2.js
@@ -1,1 +1,12 @@
-```Write a JavaScript function to check if a given number is prime or not.```
+// Write a JavaScript function to check if a given number is prime or not.
+
+function isPrime(number) {
+    if(number === 2) return true;
+    if(number % 2 === 0) return false;
+
+    for(let i = 3; i <= Math.sqrt(number); i += 2) {
+        if(number % i === 0) return false;
+    }
+
+    return true;
+}
