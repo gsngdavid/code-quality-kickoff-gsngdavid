37e937eaefdc5ece7fc7b80ca6a2e71c9ceb21cd
gsngdavid <gsngdavid@gmail.com>
2023-10-25 10:04:54 +0200

    feat: implement get array depth function and use it to flatten an array

--- src/exercise4/exercise1.js
+++ src/exercise4/exercise1.js
@@ -1,2 +1,14 @@
-```Write a function(s) that checks the level of nesting of an array.
-Then, use that function to flatten the array into a single-dimensional array.```
+// Write a function(s) that checks the level of nesting of an array.
+// Then, use that function to flatten the array into a single-dimensional array.
+
+function getArrayDepth(array) {
+  return Array.isArray(array)
+    ? 1 + Math.max(0, ...array.map((element) => getArrayDepth(element)))
+    : 0;
+}
+
+const numbers = [1, 2, 3, [4, [5, 6, [7, 8]], 9]]
+
+const depth = getArrayDepth(numbers)
+// eslint-disable-next-line no-undef
+console.log(numbers.flat(depth))
